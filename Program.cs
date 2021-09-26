e61b3c18bed5847b21257650f6320f830ef398bc
Bob Phillips <phil1630@gmail.com>
2021-09-26 14:17:38 -0400

    First Commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using Unosquare.RaspberryIO;
+
+namespace TakePic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Pi.Info);
+            Console.WriteLine("Taking picture...");
+            TestCaptureImage();
+        }
+
+        static void TestCaptureImage()
+        {
+            var pictureBytes = Pi.Camera.CaptureImageJpeg(640, 480);
+            var targetPath = "/home/pi/picture.jpg";
+            if (File.Exists(targetPath))
+                File.Delete(targetPath);
+
+            File.WriteAllBytes(targetPath, pictureBytes);
+            Console.WriteLine($"Took picture -- Byte count: {pictureBytes.Length}");
+        }
+    }
+}
